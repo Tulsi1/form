4ee97eab56f109f721f2409fbfcae4bb081cc70b
sajalchh <sajalchh@gmail.com>
2020-06-26 22:48:32 +0530

    second commit regarding mail

--- execution.php
+++ execution.php
@@ -1,55 +1,16 @@
-
-  <?php
-//   use PHPMailer\PHPMailer\PHPMailer;
-//   use PHPMailer\PHPMailer\Exception;
-//   require 'PHPMailer-master/src/Exception.php';
-//   require 'PHPMailer-master/src/PHPMailer.php';
-//   require 'PHPMailer-master/src/SMTP.php';
-
-//   $mail = new PHPMailer();
-//   $mail->IsSMTP();
-
-//   $mail->SMTPDebug  = 0;  
-//   $mail->SMTPAuth   = TRUE;
-//   $mail->SMTPSecure = "tls";
-//   $mail->Port       = 587;
-//   $mail->Host       = "smtp.gmail.com";
-//   $mail->Username   = "sajaltry2001@gmail.com";
-//   $mail->Password   = "sajal2010";
-//   if(!empty($_POST["Submit"])) {
-// 	$name = $_POST["Name"];
-// 	$email = $_POST["Email"];
-// 	$subject = $_POST["subject"];
-// 	$content = $_POST["message"];
-
-//   $mail->IsHTML(true);
-//   $mail->AddAddress("sajalchh@gmail.com", "sajal");
-//   $mail->SetFrom($email, $name);
-// //   $mail->AddReplyTo("reply-to-email", "reply-to-name");
-// //   $mail->AddCC("cc-recipient-email", "cc-recipient-name");
-//   $mail->Subject = $subject;
-
-//   $mail->MsgHTML($content); 
-//   if(!$mail->Send()) {
-//     echo "Error while sending Email.";
-//     var_dump($mail);
-//   } else {
-//     echo "Email sent successfully";
-//   }
-// }
-?>
 <?php
   require_once 'vendor/autoload.php';
 	$name = $_POST["Name"];
 	$email = $_POST["Email"];
 	$subject = $_POST["subject"];
 	$content = $_POST["message"];
-
+  $realContent = 'from:'.$name.' <'.$email.'>'.nl2br($content);
 
 // Create the Transport
 $transport = (new Swift_SmtpTransport('smtp.gmail.com', 587, 'tls'))
-  ->setUsername('sajaltry2001@gmail.com')
-  ->setPassword('sajal2010')
+  ->setUsername('cqcontactform@gmail.com')
+  //SET PASSWORD HERE
+  ->setPassword('')
 ;
 
 // Create the Mailer using your created Transport
@@ -60,10 +21,7 @@ $message = (new Swift_Message())
   ->setSubject($subject)
   ->setFrom([$email => $name])
   ->setTo(['sajaltry2001@gmail.com' => 'Sajal Chhamunya'])
-  ->setBody('From:')
-  ->addPart($name)
-  // ->addPart($email)
-  // ->addPart($content)
+  ->setBody($realContent)
   ;
 
 // Send the message
